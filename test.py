63f9258f70f55e4595817186fb6d632e463c3aed
MaksimKachaev1996@gmail.com <MaksimKachaev1996@gmail.com>
2023-11-09 21:15:01 +0300

    Update

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-def greet():
-    message = "hello"
-    new_message = message.capitalize()
-    return new_message
+def get_maximum():
+    celsius = [14, 15.1, 12.3]
+    maximum = max(celsius)
+    return maximum
 
-greeting = greet()
-print(greeting)
+
+celsius = get_maximum()
+
+fahrenheit = celsius * 1.8 + 32
+print(fahrenheit)
